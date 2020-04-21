444051ce536577864f4f0252cb12841045973d6c
Wellington <wsouza@kwipped.com>
2020-04-21 08:03:14 -0400

    Added settings page

---
+++ README.md
@@ -0,0 +1,37 @@
+#TL;DR
+For experienced programmers.
+1. Retrieve your APPROVE id from KWIPPED
+2. Download the wordpress plugin from the dist folder.
+2. Install the plugin into Wordpress
+3. The plugin provides an AJAX POST uri at admin_url('admin-ajax.php') with action get_approve_information
+4. Place the following javascript in the woocommerce cart and mini cart
+```
+//***********************************
+//* Added by KWIPPED
+//***********************************
+ 	if(window.approve_custom_code_initialized){
+		window.approve_custom_code_initialized = true;
+		function update_approve_button(){
+			var data = {'action': 'get_approve_information'};
+			jQuery.post('<?php echo admin_url('admin-ajax.php'); ?>' ,data, function(response) {
+				jQuery('[custom-approve-button]').each(function(){
+					jQuery(this).html(response.teaser);
+					jQuery(this).attr('href',response.url);
+				});
+			});
+	   }
+	   jQuery(document).ready(function() {
+		  update_approve_button();
+	   });
+	   jQuery(document.body).on( 'updated_cart_totals', function(){
+		   update_approve_button();
+	   });
+	}
+//******************************
+//* End of added by KWIPPED
+//*******************************
+```
+5.Place a button in the woocommerce cart and mini cart. (Replace your APPROVE ID)
+```
+<a href="https://www.kwipped.com/approve/finance?approveid=<your approve id>" class="gem-button gem-button-size-medium gem-button-style-flat gem-button-text-weight-normal checkout-button button alt wc-forward" target="_blank" custom-approve-button >
+```
--- woocommerce-approve-plugin.php
+++ woocommerce-approve-plugin.php
@@ -1,120 +1,161 @@
 <?php
 	/*
-	Plugin Name: Woocommerce APPROVE Plugin
+	Plugin Name: APPROVE Woocommerce Plugin
 	Plugin URI: http://kwipped.com
 	description:May be used by APPROVE clients to create the necessary link to connect into the Approve cart from a woocommerce cart.
-	Version: 1.3.7
+	Version: 1.3.8
 	Author: Wellington Souza
 	Author URI: http://kwipped.com
 	License: GPL2
 	*/
 
+	/**
+	 * Provides update functionality
+	 */
 	require 'plugin-update-checker-4.9/plugin-update-checker.php';
 	$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-		'https://github.com/KWIPPED/woocommerce-approve-plugin/',
+		'https://github.com/KWIPPED/approve-woocommerce-plugin/',
 		__FILE__,
-		'woocommerce-approve-plugin'
+		'approve-woocommerce-plugin'
 	);
 
-
-add_action("wp_ajax_get_approve_information", 'get_approve_information' );
-add_action("wp_ajax_nopriv_get_approve_information", "get_approve_information");
-
-$mode = "live";
-$landing_page_url= $mode=="test" ? "https://dev.kwipped.com/approve/finance" : "https://www.kwipped.com/approve/finance";
-$api_url= $mode=="test" ? "https://dev.kwipped.com/api/v2/approve-widget/finance-teasers/" : "https://www.kwipped.com/api/v2/approve-widget/finance-teasers/" ;
-$cacert_file= $mode=="test" ? "/usr/local/etc/openssl/cert.pem" : null;
-
-function get_approve_information() {
-	$approve = new Approve();
-
 	//*****************************************************
-	//** YOUR CODE GOES IN HERE
-	//* For each item in your cart call the following function:
-	//* $approve->add(model,price,quantity,type)
-	//* If you need information about the specific meaning of each of these fields please visit 
-	//* https://kwipped.com/someplacewhereinformationlives
+	//* Plugin settings : APPROVE Woocommerce Plugin (awcp)
 	//*****************************************************
-	$kwipped_approve_id="eyJpdiI6ImVMMk1TRWZNeE82RFA5aks0engzZGc9PSIsInZhbHVlIjoibWZpQmhldzhZNnFNRmQ2d0tnQzBuQT09IiwibWFjIjoiYWIwYWUzM2YzOGVmYWZmZjg0YTE0YzFjYjIyNDVkNGVhN2NjZjcxYTM2MDZhMzI4ZTA3YjU5YjJiYWZhYzU3MyJ9";
+	add_action( 'admin_menu', function() {
+		add_options_page( 'APPROVE Woocommerce Plugin Page', 'APPROVE Woocommerce Plugin', 'manage_options', 'approve-woocommerce-plugin', 'awcp_render_plugin_settings_page' );
+	});
+
+	function awcp_render_plugin_settings_page() {
+		?>
+		<h2>APPROVE Woocommerce Plugin Settings</h2>
+		<form action="options.php" method="post">
+			 <?php 
+			 	settings_fields( 'awcp_options' );
+				do_settings_sections( 'approve-woocommerce-plugin' ); ?>
+			 <input name="submit" class="button button-primary" type="submit" value="<?php esc_attr_e( 'Save' ); ?>" />
+		</form>
+		<?php
+  }
+
+  add_action( 'admin_init', function() {
+		register_setting( 'awcp_options', 'awcp_options');
+		add_settings_section( 'api_settings', 'API Settings', 'awcp_section_text', 'approve-woocommerce-plugin' );
+		add_settings_field( 'approve_id', 'APPROVE id', 'approve_id', 'approve-woocommerce-plugin', 'api_settings' );
+	});
 	
-	global $woocommerce;
-	$items = $woocommerce->cart->get_cart();
-	foreach($items as $item => $values) { 
-		//print_r($values); die();
-		$approve->add($values['data']->get_name(),get_post_meta($values['product_id'] , '_price', true),$values['quantity'],"new_product");
-	}
-	$shipping = $woocommerce->cart->get_shipping_total();
-	if(!empty($shipping)) $approve->add("Shipping",$shipping,1,"shipping");
-	//***************************
-	//* End of your code
-	//***************************
+	function awcp_section_text() {
+		echo '<p>Here you can set all the options for using the APPROVE Woocommerce API. Retreive your APPROVE information by logging into KWIPPED.</p>';
+  }
 
-	wp_send_json($approve->get_approve_information($kwipped_approve_id));
-	wp_die(); // this is required to terminate immediately and return a proper response
-}
-
-
-//****************************************************************************************
-//* You should not modify the code below. It assures the correct format needed by Approve.
-//*****************************************************************************************
-class Approve{
-	private $items = [];
-	private $current_total=0;
-
-	public function add($model,$price,$quantity,$type){
-		$tmp = [];
-		$tmp["model"]=$model;
-		$tmp["quantity"]=$quantity;
-		$tmp["type"]=$type;
-		//In Approve the quantity is a representation of how many items are in the total.
-		$tmp["price"]=$price;
-		$this->current_total+=($tmp["price"]*$quantity);
-		$this->items[]=(object)$tmp;
+  function approve_id() {
+		$options = get_option( 'awcp_options');
+		echo "<input id='approve_plugin_setting_api_key' name='awcp_options[approve_id]' type='text' value='".esc_attr( $options['approve_id'])."' style='width:100%;'/>";
 	}
 
+	//*****************************
+	//* Plugin settings END
+	//*****************************
 	
-	public function get_approve_information($kwipped_approve_id){
-		global $landing_page_url;
-		$teaser = "";
-		if(function_exists('curl_version')){
-			$teaser = $this->get_teaser($this->current_total,$kwipped_approve_id);
-		}
-		else{
-			$teaser = "N/A Your server does not suppor CURL requests. Please ask your system administrator to enable it.";
-		}
 
-		return [
-			"url"=>$landing_page_url."?approveid=".$kwipped_approve_id.(sizeof($this->items)>0 ? "&items=".json_encode($this->items) : null),
-			"teaser"=>$teaser
-		];
+	add_action("wp_ajax_get_approve_information", 'get_approve_information' );
+	add_action("wp_ajax_nopriv_get_approve_information", "get_approve_information");
+
+	$mode = "live";
+	$landing_page_url= $mode=="test" ? "https://dev.kwipped.com/approve/finance" : "https://www.kwipped.com/approve/finance";
+	$api_url= $mode=="test" ? "https://dev.kwipped.com/api/v2/approve-widget/finance-teasers/" : "https://www.kwipped.com/api/v2/approve-widget/finance-teasers/" ;
+	$cacert_file= $mode=="test" ? "/usr/local/etc/openssl/cert.pem" : null;
+
+	function get_approve_information() {
+		$approve = new Approve();
+
+		//*****************************************************
+		//** YOUR CODE GOES IN HERE
+		//* For each item in your cart call the following function:
+		//* $approve->add(model,price,quantity,type)
+		//* If you need information about the specific meaning of each of these fields please visit 
+		//* https://kwipped.com/someplacewhereinformationlives
+		//*****************************************************
+		$kwipped_approve_id=(get_option('awcp_options'))['approve_id'];
+		
+		global $woocommerce;
+		$items = $woocommerce->cart->get_cart();
+		foreach($items as $item => $values) { 
+			//print_r($values); die();
+			$approve->add($values['data']->get_name(),get_post_meta($values['product_id'] , '_price', true),$values['quantity'],"new_product");
+		}
+		$shipping = $woocommerce->cart->get_shipping_total();
+		if(!empty($shipping)) $approve->add("Shipping",$shipping,1,"shipping");
+		//***************************
+		//* End of your code
+		//***************************
+
+		wp_send_json($approve->get_approve_information($kwipped_approve_id));
+		wp_die(); // this is required to terminate immediately and return a proper response
 	}
 
-	//**********************************
-	//* Retreieves teasers from KWIPPED.
-	//**********************************
-	private function get_teaser($amount,$kwipped_approve_id){
-		global $api_url, $cacert_file;
-		$ch = curl_init();
 
-		curl_setopt($ch, CURLOPT_URL, $api_url.$amount);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-		if(!empty($cacert_file)) curl_setopt ($ch, CURLOPT_CAINFO, $cacert_file);
-		//var_dump(openssl_get_cert_locations());
-		$headers = array();
-		$headers[] = 'Authorization: Basic '.$kwipped_approve_id;
-		$headers[] = 'Content-Type: application/json';
-		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+	//****************************************************************************************
+	//* You should not modify the code below. It assures the correct format needed by Approve.
+	//*****************************************************************************************
+	class Approve{
+		private $items = [];
+		private $current_total=0;
 
-		$result = curl_exec($ch);
-		if (curl_errno($ch)) {
-			return 'Error:' . curl_error($ch);
+		public function add($model,$price,$quantity,$type){
+			$tmp = [];
+			$tmp["model"]=$model;
+			$tmp["quantity"]=$quantity;
+			$tmp["type"]=$type;
+			//In Approve the quantity is a representation of how many items are in the total.
+			$tmp["price"]=$price;
+			$this->current_total+=($tmp["price"]*$quantity);
+			$this->items[]=(object)$tmp;
 		}
-		curl_close($ch);
 
-		$data = json_decode($result);
-		$teaser = "Finance for $".number_format($data->lease_teaser[0]->monthly_rate,0)."/mo";
-		return $teaser;
-	}
+		
+		public function get_approve_information($kwipped_approve_id){
+			global $landing_page_url;
+			$teaser = "";
+			if(function_exists('curl_version')){
+				$teaser = $this->get_teaser($this->current_total,$kwipped_approve_id);
+			}
+			else{
+				$teaser = "N/A Your server does not suppor CURL requests. Please ask your system administrator to enable it.";
+			}
+
+			return [
+				"url"=>$landing_page_url."?approveid=".$kwipped_approve_id.(sizeof($this->items)>0 ? "&items=".json_encode($this->items) : null),
+				"teaser"=>$teaser
+			];
+		}
+
+		//**********************************
+		//* Retreieves teasers from KWIPPED.
+		//**********************************
+		private function get_teaser($amount,$kwipped_approve_id){
+			global $api_url, $cacert_file;
+			$ch = curl_init();
+
+			curl_setopt($ch, CURLOPT_URL, $api_url.$amount);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+			if(!empty($cacert_file)) curl_setopt ($ch, CURLOPT_CAINFO, $cacert_file);
+			//var_dump(openssl_get_cert_locations());
+			$headers = array();
+			$headers[] = 'Authorization: Basic '.$kwipped_approve_id;
+			$headers[] = 'Content-Type: application/json';
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+			$result = curl_exec($ch);
+			if (curl_errno($ch)) {
+				return 'Error:' . curl_error($ch);
+			}
+			curl_close($ch);
+
+			$data = json_decode($result);
+			$teaser = "Finance for $".number_format($data->lease_teaser[0]->monthly_rate,0)."/mo";
+			return $teaser;
+		}
 }
 ?>
