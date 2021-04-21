b33c5131f342ef3994e4bb1b9e841d6be7c955ae
Wellington <wsouza@kwipped.com>
2021-04-21 11:50:43 -0400

    Correction

--- approve-woocommerce-integration-plugin.php
+++ approve-woocommerce-integration-plugin.php
@@ -3,7 +3,7 @@
 	Plugin Name: APPROVE Woocommerce Integration Plugin
 	Plugin URI: http://kwipped.com
 	description:May be used by APPROVE clients to create the necessary link to connect into the Approve cart from wordpress.
-	Version: 2.5
+	Version: 2.6
 	Author: Wellington Souza
 	Author URI: http://kwipped.com
 	License: GPL2
@@ -14,7 +14,7 @@
 	
 	class ApproveWoocommerceIntegrationPlugin{
 
-		private $version = "2.5";
+		private $version = "2.6";
 		private $test = false;
 
 		function __construct(){
